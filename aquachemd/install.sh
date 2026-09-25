9db4e966c052045dfab188e0b702bd8a11b72577
sfeakes <sfeakes@gmail.com>
2026-09-25 17:41:29 -0500

    Fix AquaChemD installer wrapper branch and path

--- aquachemd/install.sh
+++ aquachemd/install.sh
@@ -5,7 +5,7 @@ tmp=$(mktemp)
 trap 'rm -f "$tmp"' EXIT
 
 curl -fsSL \
-  https://raw.githubusercontent.com/AquaDaemon/AquachemD/master/release/remote_install.sh \
+  https://raw.githubusercontent.com/aqualinkd/AquachemD/main/release/remote-install.sh \
   -o "$tmp"
 
 bash "$tmp" "$@"
